3e01c9687f77943c8ef3185d7e771f0058628598
kate115 <75540720+kate115@users.noreply.github.com>
2023-07-16 22:09:13 -0700

    large bugfix and whole coding complete!!!

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-console.log('Hello world')
+let formEl, answerEl, resultEl, submitEl;
+const answer = 75;
+
+formEl = document.getElementById('quiz');
+console.log(formEl);
+answerEl = document.getElementById('answer');
+console.log(answerEl);
+resultEl = document.getElementById('result');
+console.log(resultEl);
+
+function checkAndWrite(event) {
+    if(answerEl.value == answer) {
+        console.log('User got it correct and answerEl has value ' + answerEl.value);
+        resultEl.innerText = 'Great job! You got it correct!';
+    } else {
+        console.log('User got it incorrect and answerEl has value ' + answerEl.value);
+        resultEl.innerText = 'Sorry, that was incorrect. Try again!';
+    }
+    event.preventDefault();
+}
+
+formEl.addEventListener('submit', checkAndWrite);
